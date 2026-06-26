44b795031460fbbb7059e2c90116cb37f68891f4
Ishpreet Singh <43165439+IshpreetSingh23@users.noreply.github.com>
2026-06-27 01:25:56 +0530

    Create Searialize.java

---
+++ Week_4/Day_5/Searialize.java
@@ -0,0 +1,47 @@
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        dfs(root, sb);
+        return sb.toString();
+    }
+
+    private void dfs(TreeNode node, StringBuilder sb) {
+
+        if (node == null) {
+            sb.append("null,");
+            return;
+        }
+
+        sb.append(node.val).append(",");
+
+        dfs(node.left, sb);
+        dfs(node.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+        Queue<String> queue =
+                new LinkedList<>(Arrays.asList(data.split(",")));
+
+        return build(queue);
+    }
+
+    private TreeNode build(Queue<String> queue) {
+
+        String val = queue.poll();
+
+        if (val.equals("null")) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(val));
+
+        root.left = build(queue);
+        root.right = build(queue);
+
+        return root;
+    }
+}
